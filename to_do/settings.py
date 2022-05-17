928f5c9f1bd148d57d95129c6b254d52fc3a54d0
BorisToboltsov <b.toboltsov@vcore.su>
2022-05-17 14:32:21 +0300

    lesson-1. Hidden SECRET_KEY

--- to_do/settings.py
+++ to_do/settings.py
@@ -27,7 +27,7 @@ except FileNotFoundError:
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = secret.get("SECRET_KEY")
-print(SECRET_KEY)
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
