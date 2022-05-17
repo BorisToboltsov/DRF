c0a4678d2558d56e8eddf9775215c0377c9f791a
BorisToboltsov <b.toboltsov@vcore.su>
2022-05-17 14:31:57 +0300

    lesson-1. Hidden SECRET_KEY

--- to_do/settings.py
+++ to_do/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+import json
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -19,10 +19,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
+try:
+    with open("/home/boris/DRF/to_do/to_do/secret.json", "r") as f:
+        secret = json.load(f)
+except FileNotFoundError:
+    raise "Файл secret.json не найден"
+
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = (
-    "django-insecure-zy!$g%i1qkm4rg4)v9d4)z=#_mzqikw)mdm7tq9big@^7_2ef^"
-)
+SECRET_KEY = secret.get("SECRET_KEY")
+print(SECRET_KEY)
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
