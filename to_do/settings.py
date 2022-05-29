ee381c3177cf7646ba9cbbc26ceb665ac0ef8f40
BorisToboltsov <b.toboltsov@vcore.su>
2022-05-29 22:24:10 +0300

    Lesson-3. fix settings

--- to_do/settings.py
+++ to_do/settings.py
@@ -19,15 +19,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-# try:
-#     with open("/home/boris/DRF/to_do/to_do/secret.json", "r") as f:
-#         secret = json.load(f)
-# except FileNotFoundError:
-#     raise "Файл secret.json не найден"
-#
-# # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = secret.get("SECRET_KEY")
-SECRET_KEY = "dsasad"
+try:
+    with open("/home/boris/DRF/to_do/to_do/secret.json", "r") as f:
+        secret = json.load(f)
+except FileNotFoundError:
+    raise "Файл secret.json не найден"
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = secret.get("SECRET_KEY")
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
